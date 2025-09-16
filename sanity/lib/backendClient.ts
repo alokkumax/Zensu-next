cecc5a62583b257bb4cb197000f3bedf75fa2897
alokkumax <alokkumax1@gmail.com>
2025-09-16 23:25:56 +0530

    fix hardcode

--- sanity/lib/backendClient.ts
+++ sanity/lib/backendClient.ts
@@ -3,9 +3,9 @@ import { createClient } from "next-sanity";
 import { apiVersion, dataset, projectId } from "../env";
 
 export const backendClient = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION!,
+  projectId: 'woweegpk',
+  dataset: 'production',
+  apiVersion: '2025-09-16',
   useCdn: false,
 })
 // export const client = createClient({
